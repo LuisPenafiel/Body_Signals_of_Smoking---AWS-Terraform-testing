1cf06684ff0aa61d27b11299e8ad222937f546e5
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2025-07-05 16:05:40 +0000

    comment all lines output and variables

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,3 +1,3 @@
-resource "null_resource" "test" {}
-
-# prueba
+# resource "null_resource" "test" {}
+# 
+# # prueba
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,35 @@
+# # /terraform/variables.tf
+# 
+# variable "region" {
+#   description = "AWS region"
+#   default     = "eu-central-1"  # Fráncfort, Alemania (ideal para Múnich)
+# }
+# 
+# variable "env" {
+#   description = "Environment name"
+#   default     = "dev"
+# }
+# 
+# variable "db_password" {
+#   description = "Password for the RDS database"
+#   type        = string
+#   sensitive   = true
+# }
+
 # /terraform/variables.tf
 
-variable "region" {
-  description = "AWS region"
-  default     = "eu-central-1"  # Fráncfort, Alemania (ideal para Múnich)
+variable "AWS_REGION" {
+  description = "Región de AWS"
+  type        = string
 }
 
-variable "env" {
-  description = "Environment name"
-  default     = "dev"
+variable "AWS_ACCESS_KEY_ID" {
+  description = "ID de clave de acceso de AWS"
+  type        = string
 }
 
-variable "db_password" {
-  description = "Password for the RDS database"
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Clave secreta de acceso de AWS"
   type        = string
   sensitive   = true
 }
