b6234bfb6f2301cc3dfe652624893c5c9fad0181
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2025-07-05 11:16:53 +0000

    Añadí instalación de Terraform a post-create.sh y mejoré devcontainer.json

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,3 +11,13 @@ rm -rf awscliv2.zip aws
 
 # Verificar que AWS CLI esté instalado
 aws --version
+
+# Instalar Terraform (versión reciente: 1.9.5 como ejemplo, ajusta según necesites)
+echo "Installing Terraform"
+wget -P /tmp https://releases.hashicorp.com/terraform/1.9.5/terraform_1.9.5_linux_amd64.zip
+unzip /tmp/terraform_1.9.5_linux_amd64.zip -d /tmp
+sudo mv /tmp/terraform /usr/local/bin/
+rm -f /tmp/terraform_1.9.5_linux_amd64.zip
+
+# Verificar que Terraform esté instalado
+terraform --version
