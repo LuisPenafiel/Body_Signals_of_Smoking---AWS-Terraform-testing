52092b06519d607be157feba6b10491fc3f694b7
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2025-07-07 14:07:18 +0000

    Corrigí duplicación de variables en main.tf para Día 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,3 +178,5 @@ resource "aws_route_table_association" "public_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+#prueba1
