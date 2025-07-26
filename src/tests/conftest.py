d31d361f6172a02a80752134bdba04aad3ce5f04
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2025-07-26 14:51:28 +0000

    Crear fixtures para pruebas (Día 8)

---
+++ src/tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from unittest.mock import Mock
+
+@pytest.fixture
+def mock_model():
+    model = Mock()
+    model.predict.return_value = [1]  # Simula una predicción de "Smoker"
+    model.feature_names_in_ = ['gender', 'Gtp', 'hemoglobin', 'height(cm)', 'triglyceride', 'waist(cm)', 'LDL', 'HDL',
+                               'Cholesterol', 'ALT', 'fasting blood sugar', 'systolic', 'AST', 'relaxation', 'weight(kg)',
+                               'age', 'serum creatinine', 'eyesight(left)', 'eyesight(right)', 'tartar', 'dental caries',
+                               'Urine protein', 'hearing(left)', 'hearing(right)']  # Todas las variables de tu modelo
+    return model
+
+@pytest.fixture
+def mock_scaler():
+    scaler = Mock()
+    scaler.transform.return_value = [[0.5] * 24]  # Datos falsos escalados (24 variables)
+    return scaler
+
+@pytest.fixture
+def mock_s3_client():
+    s3_client = Mock()
+    s3_client.download_file = Mock()
+    return s3_client
